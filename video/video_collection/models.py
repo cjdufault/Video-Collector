cdbf04dce13bde000902d6dc1e091ceaf304e183
cjdufault <dufaultconnor@gmail.com>
2020-11-17 11:05:20 -0600

    add home.html, base.html, style.css

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.pk} -- {self.name} -- {self.url} -- {self.notes[:200]}'
