ecdfac58de1b5dbdc7bbe04c1f99b7521cca1462
cjdufault <dufaultconnor@gmail.com>
2020-11-17 14:25:09 -0600

    add video_list page w/ embedded videos

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from urllib import parse
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -6,6 +8,38 @@ class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+    
+    # overrides inherited save method
+    def save(self, *args, **kwargs):
+        # check for valid YT url and extract video id
+        # don't save if not valid
+        try:
+            url_components = parse.urlparse(self.url)
+            
+            # check for valid URL
+            invalid_scheme = url_components.scheme != 'https'
+            invalid_netloc = url_components.netloc != 'www.youtube.com'
+            invalid_path = url_components.path != '/watch'
+            if invalid_scheme or invalid_netloc or invalid_path:
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            
+            query_string = url_components.query
+            if not query_string:    # check that there is a query
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            
+            parameters = parse.parse_qs(query_string, strict_parsing=True)
+            parameter_list = parameters.get('v')
+            if not parameter_list:  # check that there is a video id
+                raise ValidationError(f'Invalid YouTube parameters {self.url}')
+            
+            self.video_id = parameter_list[0]
+            
+        except ValueError as e:
+            raise ValidationError(f'Couldn\'t parse URL {self.url}') from e
+        
+        # save to db
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return f'{self.pk} -- {self.name} -- {self.url} -- {self.notes[:200]}'
--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,7 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm, SearchForm
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.db.models.functions import Lower
 
 # Create your views here.
 
 def home(request):
     app_name = 'Linguistics Videos'
     return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+
+# add a video
+def add(request):
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        
+        if new_video_form.is_valid():
+            try:
+                new_video_form.save() # save if form is valid
+                return redirect('video_list')
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'Video already added')
+            
+        messages.warning(request, 'Invalid! Check your data!')
+        
+        # show page with previously entered form data
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+        
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+
+# display list of saved Videos
+def video_list(request):
+    
+    search_form = SearchForm(request.GET)
+    
+    if search_form.is_valid():
+        # only list videos matching the search term
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(name__icontains=search_term).order_by(Lower('name'))
+        
+    else:
+        search_form = SearchForm()
+        videos = Video.objects.order_by(Lower('name'))
+        
+    return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form})
+    
